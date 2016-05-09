a94fd3a52a9898b8e89020a7b8754e52ea00d664
florije1988 <florije1988@gmail.com>
2016-05-09 17:39:50 +0800

    提交其他的东西。

---
+++ manage_gunicorn.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = 'florije'
+
+import os
+import sys
+
+from flask.ext.script import Command, Option
+
+
+class GunicornServer(Command):
+    """Run the app within Gunicorn"""
+
+    def get_options(self):
+        from gunicorn.config import make_settings
+
+        settings = make_settings()
+        options = (
+            Option(*klass.cli, action=klass.action)
+            for setting, klass in settings.iteritems() if klass.cli
+        )
+        return options
+
+    def run(self, *args, **kwargs):
+        run_args = sys.argv[2:]
+        run_args.append('manage:app')
+        os.execvp('gunicorn', [''] + run_args)
